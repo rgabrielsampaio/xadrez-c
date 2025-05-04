d22b395db032bc49447713a712cb41910c1a3a22
Renato Gabriel <206131104+rgabrielsampaio@users.noreply.github.com>
2025-05-04 11:56:17 -0300

    Movimentação de peças de xadrez - Nível aventureiro
    Adicionei a peça do Cavalo, juntamente com a lógica da sua movimentação.

--- xadrez.c
+++ xadrez.c
@@ -5,29 +5,50 @@ int main(){
     int torre;
     int bispo = 0;
     int rainha = 0;
+    int cavaloBaixo = 0;
+    int cavaloEsq;
 
     printf("Movimentação da Torre\n");
 
-    for (torre = 0; torre <= 4; torre++)
+    for (torre = 0; torre < 5; torre++) // Movimentar 5 casas pra direita.
     {
-        printf("Torre andou pra direita\n");
+        printf("Direita\n");
     }
+
+    printf("\n");
+
 
     printf("Movimentação do Bispo\n");
 
-    while (bispo <= 4)
+    while (bispo < 5) // Movimentar 4 casas em diagonal.
     {
-        printf("Bispo andou pra cima\n");
-        printf("Bispo andou pra direita\n");
+        printf("Baixo\n");
+        printf("Direita\n");
         bispo++;
     }
 
+    printf("\n");
+
+
     printf("Movimentação da Rainha\n");
     
-    do {
-        printf("Rainha andou pra esquerda\n");
+    do { // Movimentar 8 casas pra esquerda.
+        printf("Esquerda\n");
         rainha++;
-    } while (rainha <= 7);
+    } while (rainha < 8);
 
-    return 0;
+    printf("\n");
+
+    printf("Movimentação do Cavalo\n");
+
+    for (cavaloEsq = 0; cavaloEsq < 1; cavaloEsq++) { // Movimentar 1 casa pra esquerda.
+        while (cavaloBaixo < 2) // Movimentar 2 casas pra baixo.
+        {
+            printf("Baixo\n");
+            cavaloBaixo++;
+        }
+        printf("Esquerda\n");
+    }
+
+        return 0;
 }
