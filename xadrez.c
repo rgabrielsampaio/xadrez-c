601eff94606e740d50ef36b8f76bb7a2629473a3
Renato Gabriel Sampaio <206131104+rgabrielsampaio@users.noreply.github.com>
2025-05-08 12:03:15 -0300

    Desafio xadrez - Nível mestre concluído

--- xadrez.c
+++ xadrez.c
@@ -1,51 +1,92 @@
 #include <stdio.h>
 
-// LOOP EXTERNO --> CONTROLA A LINHA
-// LOOP INTERNO --> CONTOLA ELEMENTOS QUE ESTÃO NA LINHA
+// Variáveis constantes
+const int casasTorre = 5;
+const int casasBispo = 5;
+const int casasRainha = 8;
 
-void movimentoBispo(int j, int i) 
+// Função recursiva para mover a Torre (direita)
+void moverTorre(int casas)
 {
-    j = 4;
-    i = 4;
-    if (i == 0 && j == 0)
+    if (casas > 0)
     {
-        continue;
+        printf("Torre move-se 1 casa para a Direita\n");
+        moverTorre(casas - 1);
     }
-    
-    for (int i; i > 0; i--)
+}
+
+// Função recursiva para mover o Bispo em diagonais
+void moverBispo(int casas)
+{
+    if (casas > 0)
     {
-        printf("Cima\n");
-        if (j > 0)
+        for (int v = 0; v < 1; v++) // Loop externo (vertical)
         {
-            printf("Direita\n");
-            j--;
+            for (int h = 0; h < 1; h++) // Loop externo (horizontal)
+            {
+                printf("Bispo move-se 1 casa para Cima\n");
+                printf("Bispo move-se 1 casa para a Direita\n");
+            }
+        }
+        moverBispo(casas - 1);
+    }
+}
+
+// Função recursiva para mover a Rainha (esquerda)
+void moverRainha(int casas)
+{
+    if (casas > 0)
+    {
+        printf("Rainha move-se 1 casa para a Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
+// Função com loops complexos para o Cavalo
+void moverCavalo()
+{
+    int movimentos = 1;
+
+    for (int i = 0; i < movimentos; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (j < 2)
+            {
+                printf("Cavalo move-se para Cima\n");
+            }
+            else if (j == 2)
+            {
+                if (i == 0 && j == 2)
+                {
+                    printf("Cavalo move-se para a Direita\n");
+                    break;
+                }
+            }
         }
     }
 }
 
-int main() {
+int main()
+{
 
-    printf("\n");
-    printf("Movimento do Cavalo\n");
-    printf("\n");
-
-    // LOOP DO CAVALO
-    for (int i = 1; i > 0; i--) 
-    {
-        for (int j = 2; j > 0; j--)
-        {
-            printf("Cima\n");
-        }
-        printf("Direita\n");
-    }  
-
-    printf("\n");
-    printf("Movimento do Bispo\n");
+    printf("\nMovimentação da Torre\n\n");
+    moverTorre(casasTorre);
     printf("\n");
 
+    printf("\nMovimentação do Bispo\n\n");
+    moverBispo(casasBispo);
+    printf("\n");
 
-    // LOOP DO BISPO
+    printf("\nMovimentação da Rainha\n\n");
+    moverRainha(casasRainha);
+    printf("\n");
 
+    printf("\nMovimentação do Cavalo\n\n");
+    moverCavalo();
+    printf("\n");
+
+    printf("\nFeito por: Renato Gabriel Sampaio de Carvalho\n\n");
 
     return 0;
 }
